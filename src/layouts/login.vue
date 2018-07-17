38a9b034d7fb2a142319ea8d9f3179a6dbe977f0
hemlk <truonglehiep@gmail.com>
2018-07-17 17:03:36 +0700

    clientside auth

---
+++ src/layouts/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    This is the Login page
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,23 +1,30 @@
 import _404 from 'pages/404'
 import defaultLayout from 'layouts/default'
+import loginLayout from 'layouts/login'
 import pgProducts from 'pages/products'
 import pgSuppliers from 'pages/suppliers'
 export default [
   {
+    path: '/login',
+    component: loginLayout,
+  },
+  {
     path: '/',
     component: defaultLayout,
+    meta: {requiresAuth: true},
     children: [
       {
         path: 'products',
         component: pgProducts,
+        meta: {requiresAuth: true},
       },
       {
         path: 'suppliers',
         component: pgSuppliers,
+        meta: {requiresAuth: true},
       },
     ],
   },
-
   {
     // Always leave this as last one
     path: '*',
