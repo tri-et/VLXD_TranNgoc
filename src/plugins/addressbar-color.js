920daaf1b148570ac7865177181cef82fb81114b
hemlk <truonglehiep@gmail.com>
2018-07-18 17:55:04 +0700

    fine tune login page
    add in status bar coloring ...

---
+++ src/plugins/addressbar-color.js
@@ -0,0 +1,8 @@
+// import something here
+import {AddressbarColor} from 'quasar'
+
+// leave the export, even if you don't use it
+export default () => {
+  // something to do
+  AddressbarColor.set('#a2e3fa')
+}
